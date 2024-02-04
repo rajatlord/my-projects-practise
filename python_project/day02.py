d0d90e47096c65de6e398a6443bba7340efa471a
rajatlord <rajatmathur1881@gmail.com>
2024-02-05 05:05:52 +0530

    object

--- python_project/day02.py
+++ python_project/day02.py
@@ -6,3 +6,20 @@ print
 lambda '''
 print("hey" , 6 , 7 , sep ="~" , end="666\n")
 print("Rajat")
+#Variables and data types 
+a = complex(4 , 5)
+b = "Rajat"
+c = True
+print("The type of a " ,type(a))
+print("The type of b " ,type(b))
+print("The type of c " ,type(c))
+
+list1 = [3,5,7 , [-4 , 5], ["Rajat" , "light"]]
+print(list1)
+
+tuple1 = (("parrot" , "bird") , ("lion" , "Tiger"))
+print(tuple1)
+
+dict1 = {"name" : "Light" , "age":29, "canVote" : True}
+print(dict1)
+# They all are object 
