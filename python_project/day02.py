1bf853b7170e7620c390160451374a5da602443f
rajatlord <rajatmathur1881@gmail.com>
2024-02-04 21:45:57 +0530

    today i learn about print statment

---
+++ python_project/day02.py
@@ -0,0 +1,8 @@
+# comment
+print("\"hello\" here i am again \n hi there new line")
+print("line comment ") # hi there
+''' KeyboardInterrupt
+print
+lambda '''
+print("hey" , 6 , 7 , sep ="~" , end="666\n")
+print("Rajat")
