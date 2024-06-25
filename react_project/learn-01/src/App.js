763964b0d31b6e29620869a9310a26c21423b0a5
Rajat <rajatmathur1881@gmail.com>
2024-06-25 11:15:30 +0530

    here is react icon tutorial

--- react_project/learn-01/src/App.js
+++ react_project/learn-01/src/App.js
@@ -1,6 +1,6 @@
 import "./index.css"
-
-
+import { BsFillCartCheckFill } from "react-icons/bs";
+import { MdFamilyRestroom } from "react-icons/md";
 
 
 function App() {
@@ -11,15 +11,16 @@ function App() {
           Edit <code >src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-3xl	font-black	"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React<BsFillCartCheckFill /><MdFamilyRestroom />
+
         </a>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
+        <h1 className="font-black text-9xl">
+      Hello world!<BsFillCartCheckFill />
     </h1>
       </header>
       
