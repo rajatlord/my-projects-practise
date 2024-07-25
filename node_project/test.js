c96e935936a24ce755d4ca99d7b82e97d0cac2a8
Rajat <rajatmathur1881@gmail.com>
2024-07-25 12:28:31 +0530

    new code

--- node_project/test.js
+++ node_project/test.js
@@ -30,52 +30,49 @@
 //   console.log('Server running at http://127.0.0.1:3000/');
 // });
 
-                // These comments are added on wed 24 July 2024 
+// These comments are added on wed 24 July 2024
 
-                // console.log("Hello world ");
+// console.log("Hello world ");
 
-                // const http = require("http");
+// const http = require("http");
 
-                // const server = http.createServer((req, res) => {
-                //   res.statusCode = 200;
-                //   res.setHeader("Content-Type", "text/plain");
-                //   res.end("Hello, World!\n");
-                // });
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello, World!\n");
+// });
 
-                // const port = 3000;
-                // server.listen(port, () => {
-                //   console.log(`Server running at http://localhost:${port}/`);
-                // });
+// const port = 3000;
+// server.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
 
+// // Event Loop and Asynchronous Operations:
+// console.log("Start");
 
-                // // Event Loop and Asynchronous Operations:
-                // console.log("Start");
+// setTimeout(() => {
+//   console.log("Timeout callback executed");
+// }, 0);
 
-                // setTimeout(() => {
-                //   console.log("Timeout callback executed");
-                // }, 0);
+// console.log("End");
 
-                // console.log("End");
+// // Non-blocking I/O
+// const fs = require('fs');
 
+// console.log('Start reading file');
 
-                // // Non-blocking I/O
-                // const fs = require('fs');
+// fs.readFile('example.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error('Error reading file:', err);
+//     return;
+//   }
+//   console.log('File contents:', data);
+// });
 
-                // console.log('Start reading file');
+// console.log('End of script');
 
-                // fs.readFile('example.txt', 'utf8', (err, data) => {
-                //   if (err) {
-                //     console.error('Error reading file:', err);
-                //     return;
-                //   }
-                //   console.log('File contents:', data);
-                // });
-
-                // console.log('End of script');
-
-// what is non-blocking exactly 
-// In the context of Node.js and asynchronous programming, "non-blocking" refers to the ability of a system to perform operations without waiting for a particular task to complete before moving on to the next one. 
-
+// what is non-blocking exactly
+// In the context of Node.js and asynchronous programming, "non-blocking" refers to the ability of a system to perform operations without waiting for a particular task to complete before moving on to the next one.
 
 // =============================================================================================================================================
 // --------------------------------------------------------New code written on 24 July 2024-----------------------------------------------------
@@ -111,32 +108,31 @@
 // ----------------------------------------------------------3. Creating an HTTP Client---------------------------------------------------------
 //______________________________________________________________________________________________________________________________________________
 // http-client.js
-const http = require('http');
+const http = require("http");
 
 const options = {
-  hostname: 'jsonplaceholder.typicode.com',
+  hostname: "jsonplaceholder.typicode.com",
   port: 80,
-  path: '/posts/1',
-  method: 'GET',
+  path: "/posts/1",
+  method: "GET",
 };
 
-const req = http.request(options, res => {
-  let data = '';
+const req = http.request(options, (res) => {
+  let data = "";
 
   // A chunk of data has been received.
-  res.on('data', chunk => {
+  res.on("data", (chunk) => {
     data += chunk;
   });
 
   // The whole response has been received.
-  res.on('end', () => {
+  res.on("end", () => {
     console.log(`Response: ${data}`);
   });
 });
 
-req.on('error', error => {
+req.on("error", (error) => {
   console.error(`Error: ${error.message}`);
 });
 
 req.end();
-
